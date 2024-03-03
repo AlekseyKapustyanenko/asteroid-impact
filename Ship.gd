6ae0cf9a959cb84c601542bd37ad20d25e693da1
Aleksey Kapustyanenko <kapustyanenko@mercurydevelopment.com>
2024-03-03 19:59:15 +0400

    Add limits

--- Ship.gd
+++ Ship.gd
@@ -6,6 +6,8 @@ const navigationStep = 10;
 const positionStep = 2;
 @onready var _targetPosition = position;
 
+@export var highestPosition = 0.0
+@export var lowestPosition = 0.0
 
 func _physics_process(delta: float) -> void:	
 	var direction := get_direction(_targetPosition.y-position.y)
@@ -16,9 +18,9 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if(Input.is_action_just_pressed("down")):
+	if(_targetPosition.y<lowestPosition && Input.is_action_just_pressed("down")):
 		_targetPosition.y+=navigationStep
-	if(Input.is_action_just_pressed("up")):
+	if(_targetPosition.y>highestPosition && Input.is_action_just_pressed("up")):
 		_targetPosition.y-=navigationStep
 		var x :=7
 		
